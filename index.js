05fe07443dd4ea788d487a56253cec74a90407d2
Heather Noyes <heatherknoyes@gmail.com>
2022-04-27 18:28:08 -0400

    Separated out the code between generateMarkdown.js and index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const inquire = require("inquirer");
 const fs = require("fs");
+const gm = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -29,34 +30,7 @@ const questions = [
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-  const template = `
-<!DOCTYPE html>
-<html lang=“en”>
-<head>
-  <meta charset=“UTF-8">
-  <meta http-equiv=“X-UA-Compatible” content=“IE=edge”>
-  <meta name=“viewport” content=“width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-  <title>Node Mini Project</title>
-</head>
-<body>
-  <div class="jumbotron jumbotron-fluid">
-  <div class="container">
-    <h1 class="display-4">Hi! My name is ${response.name}</h1>
-    <p class="lead">I am from ${response.location}</p>
-    <h3>Contact Me</h3>
-    <ul class="list-group">
-      <li class="list-group-item">My favorite food is: ${response.favfood}</li>
-      <li class="list-group-item">My GitHub is: <a href="https://github.com/${response.github}" target="_blank" class="github">GitHub</a></li>
-      <li class="list-group-item">LinkedIn: <a href="https://www.linkedin.com/in/${response.linkedin}" target="_blank" class="linkedin">${response.linkedin}</a></li>
-    </ul>
-  </div>
-
-  <script src=“index.js”></script>
-</body>
-</html>`;
-
-  fs.writeFile(filename, template, (err) =>
+  fs.writeFile(fileName, gm.generateMarkdown(data), (err) =>
     err ? console.log(err) : console.log("Success!")
   );
 }
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -15,6 +15,33 @@ function generateMarkdown(data) {
   return `# ${data.title}
 
 `;
+
+  const template = `
+<!DOCTYPE html>
+<html lang=“en”>
+<head>
+  <meta charset=“UTF-8">
+  <meta http-equiv=“X-UA-Compatible” content=“IE=edge”>
+  <meta name=“viewport” content=“width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <title>Node Mini Project</title>
+</head>
+<body>
+  <div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">Hi! My name is ${response.name}</h1>
+    <p class="lead">I am from ${response.location}</p>
+    <h3>Contact Me</h3>
+    <ul class="list-group">
+      <li class="list-group-item">My favorite food is: ${response.favfood}</li>
+      <li class="list-group-item">My GitHub is: <a href="https://github.com/${response.github}" target="_blank" class="github">GitHub</a></li>
+      <li class="list-group-item">LinkedIn: <a href="https://www.linkedin.com/in/${response.linkedin}" target="_blank" class="linkedin">${response.linkedin}</a></li>
+    </ul>
+  </div>
+
+  <script src=“index.js”></script>
+</body>
+</html>`;
 }
 
 module.exports = generateMarkdown;
